3bcc1a72d3ab6170563de529347f53cb27f30966
Caio Henrique Ribeiro Martins <caio-h-rm@hotmail.com>
2016-07-05 12:03:18 -0300

    Adicionado emissão banrisul

--- application/controllers/boletos.php
+++ application/controllers/boletos.php
@@ -6,7 +6,7 @@
  * Time: 19:41
  */
 
-use OpenBoleto\Banco\BancoDoBrasil;
+use OpenBoleto\Banco\Banrisul;
 use OpenBoleto\Agente;
 class boletos extends CI_Controller
 {
@@ -16,11 +16,11 @@ class boletos extends CI_Controller
         $sacado = new Agente('Fernando Maia', '023.434.234-34', 'ABC 302 Bloco N', '72000-000', 'Brasília', 'DF');
         $cedente = new Agente('Caio Henrique', '02.123.123/0001-11', 'CLS 403 Lj 23', '71000-000', 'Brasília', 'DF');
 
-        $boleto = new BancoDoBrasil(array(
+        $boleto = new Banrisul(array(
             // Parâmetros obrigatórios
             'dataVencimento' => new DateTime('2013-01-24'),
             'valor' => 23.00,
-            'sequencial' => 1234567, // Para gerar o nosso número
+            'sequencial' => 9274, // Para gerar o nosso número
             'sacado' => $sacado,
             'cedente' => $cedente,
             'agencia' => 1724, // Até 4 dígitos
@@ -28,7 +28,8 @@ class boletos extends CI_Controller
             'conta' => 10403005, // Até 8 dígitos
             'convenio' => 1234, // 4, 6 ou 7 dígitos
         ));
-
+        //echo $boleto->gerarNossoNumero();
         echo $boleto->getOutput();
+        //echo static::modulo10Banrisul("00009274");
     }
 }
